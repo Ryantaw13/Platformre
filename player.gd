d4730ee897dd86f95c1dabf156345aa2438a791c
Ryantaw13 <ryan@tawileh.net>
2025-09-26 15:52:54 -0400

    Added flip on movement

--- player.gd
+++ player.gd
@@ -1,24 +1,33 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D 
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -450.0
 
+# ----------------- THE FIX IS HERE -----------------
+# Change 'const' to '@onready var'
+@onready var GRAVITY_STRENGTH: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+# ---------------------------------------------------
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		# Apply gravity directly to the Y component of the velocity.
+		velocity.y += GRAVITY_STRENGTH * delta 
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		# Code for flipping the sprite
+		sprite.flip_h = direction < 0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, 12)
 
